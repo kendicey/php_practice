d278b7dcef2a7681e576aaa16a91db8b13288239
kendice <kendiceyeungnm@gmail.com>
2023-04-22 22:43:21 -0700

    echo html elements with php variables (basic)

---
+++ learningPHP_challenge1_1.php
@@ -0,0 +1,15 @@
+<?php
+$name = "Teresa";
+$city = "NYC";
+$movie = "Star Wars";
+$friend = "George";
+$candy = "Sour Patch Kids";
+
+echo "<h1>Hello $name!</h1>";
+
+echo "<p>So glad you can join us in $city today! You will be <b>$friend's</b> guide as you do a range of activities.";
+
+echo "<p>You will end your day by seeing <i>$movie</i> at 7:00pm. Make sure to bring the money you've been provided. The movie ticket is \$12. $friend's favorite candy is $candy, so grab some of those too.</p>";
+
+echo "...and of-course, \"May the Force be with You!\"";
+?>
